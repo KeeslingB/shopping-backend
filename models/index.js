931a5f336fbbcde369233a3710c62d5b09766458
unknown <Breke64@gmail.com>
2023-11-17 22:07:13 -0500

    worked on relations may still need work and got started into routes

--- models/index.js
+++ models/index.js
@@ -6,11 +6,43 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+
+// Categories have many Products  // might want to use product_name instead here
+
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+});
+
+// Products belongToMany Tags (through ProductTag) 
+
+Product.belongsToMany(Tag, { through: ProductTag});
+
+Tag.belongsToMany(Product, { through: ProductTag});
+
+
+
+
 
 // Tags belongToMany Products (through ProductTag)
+
+
+
+
+// Product.belongsToMany(Tag, {
+//   foreignKey: 'product_id',
+// });
+
+
+
+// Tag.belongToMany(Product, {
+//   foreignKey: 'product_tag',
+// });
+
+
 
 module.exports = {
   Product,
